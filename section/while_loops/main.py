957f07ee0cc3cf66c0a2d735cc57ab496975513e
caskoay3012 <>
2026-02-22 02:59:48 +0000

    While Loops

--- section/while_loops/main.py
+++ section/while_loops/main.py
@@ -1,2 +1,15 @@
 start_number = 5
 countdown_values = []
+
+while start_number > 0:
+    # Append start_number to the list "countdown_values":
+    countdown_values.append(start_number)
+    
+    # Print the list "countdown_values":
+    print(f"Discount countdown complete! {countdown_values}")
+
+    #Decrease the start_number by 1 for the next loop:
+    start_number -= 1
+    
+
+    
